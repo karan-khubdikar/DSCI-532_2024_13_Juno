3fcf43d0486d2716c4d1f4b1f69077e8732af75f
Riya Eliza Shaju <riyaelizashaju@gmail.com>
2024-04-04 14:14:12 +0530

    Pie chart creation (#40)
    * Section 2 (data description) for m1_proposal.md
    
    * testing pie chart
    
    * making chart on ipynb
    
    * Creating a filtered, new dataset for the pie chart
    
    * Basic piechart created
    
    * Trying for different years (2016, 2017 etc)
    
    * Basic notebook for pie chart complete
    
    * Testing pie chart
    
    * Fixing drop down
    
    * Chart rendered when deployed, now fixing filtering function
    
    * Chart layout fixed, working on filtering
    
    * Tried and tested code for app.py, working with base python code
    
    * Filtering to be fixed, rest done
    
    * Updated file, code to get unique values of each column for the drop down
    
    * Stationary chart created, with filtering function. The function has to be debugged
    
    * Environment updated
    
    * Environment fixed, apps run and tested
    
    * Added updates for pie-chart in reflections.md
    
    * requirements.txt created
    
    ---------
    
    Co-authored-by: RIYA ELIZA <riya2023@ubc.student.ca>

---
+++ reports/m2-reflection.md
@@ -0,0 +1,3 @@
+## Reflections
+# 1. Pie Chart:
+Instead of creating side by side charts for men and women, a single chart has been created with the freedom to choose the year and the industry to see the percentage of women and men working in a particular inductry given the year. 
---
+++ src/pie_chart.py
@@ -0,0 +1,45 @@
+from dash import Dash, dcc, callback, Output, Input, html
+import dash_bootstrap_components as dbc
+import dash_vega_components as dvc
+import altair as alt
+#from vega_datasets import data
+import pandas as pd
+
+df = pd.read_csv('data/filtered/pie_chart_data.csv')
+
+
+# Initiatlize the app
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+# Layout
+app.layout = dbc.Container([
+    dvc.Vega(id='pie-chart', spec={}),
+    html.P(html.Br()),
+    html.Label('Select a Year'),
+    dcc.Dropdown(id='year', options=df["REF_DATE"].unique().tolist(), value = 2016),
+    html.Label('Select an Industry'),
+    dcc.Dropdown(id='industry', options = df["Industry"].unique().tolist(), value ='Finance')
+])
+
+# Server side callbacks/reactivity
+@callback(
+    Output('pie-chart', 'spec'),
+    Input('year', 'value'),
+    Input('industry', 'value'),
+)
+
+def create_chart(industry, year):
+
+    # Issue is with industry and year datatype - not recognised by pandas, so it returns empty df (filtered_df)
+    filtered_df = df[(df["Industry"] == "Finance") & (df["REF_DATE"] == 2016)]
+
+    chart = alt.Chart(filtered_df).mark_arc().encode(
+        theta="VALUE",
+        color="Gender"
+    )
+
+    return chart.to_dict()
+
+# Run the app/dashboard
+if __name__ == '__main__':
+    app.run(debug=True)
