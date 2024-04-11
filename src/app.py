2d95463b09af28ab1adff17cc1af66d506d1f6c5
Riya Eliza Shaju <riyaelizashaju@gmail.com>
2024-04-10 19:56:24 -0700

    Fixing line chart (#76)
    * replicated app.py code
    
    * replicated chart
    
    * required chart created, now trying to create on dashboard
    
    * created line chart with dotted line for national ratio and dynamic provincial ratio
    
    * line chart created as suggested, scope to improve chart - can reduce space near 2016
    
    * Aesthetic changes

--- src/app.py
+++ src/app.py
@@ -195,19 +195,33 @@ def update_chart(year, province):
 def create_chart(prov, selected_year):
     filtered_df = df[(df["GEO"] == prov)]
 
-    chart = alt.Chart(filtered_df).mark_line().encode(
-        x=alt.X('REF_DATE:O', axis=alt.Axis(title='Year')),
-        y=alt.Y('VALUE:Q', axis=alt.Axis(title='Number of People')),
-        color='Gender:N',
-        tooltip=['Gender:N', 'VALUE:Q']
+    canada_total = {
+    'Year': [2016, 2017, 2018, 2019, 2020],
+    'Ratio': [0.422879, 0.424134, 0.432834, 0.433269, 0.457492]
+    }
+    canada_total_ratio = pd.DataFrame(canada_total)
+
+    prov_ratio_values = filtered_df.apply(lambda row: row['VALUE'] / filtered_df.loc[(filtered_df['REF_DATE'] == row['REF_DATE']) & (filtered_df['Gender'] == 'Men'), 'VALUE'].iloc[0], axis=1)
+    prov_ratio = prov_ratio_values[prov_ratio_values != 1]
+
+    province_gender_ratio = pd.DataFrame({
+    'Year': range(2016, 2021),
+    'Ratio': prov_ratio
+    })
+
+    chart = alt.Chart(province_gender_ratio).mark_line().encode(
+        x=alt.X('Year:O', axis=alt.Axis(title='Year')),
+        y=alt.Y('Ratio:Q', axis=alt.Axis(title='Ratio (Women/Men)')),
+        tooltip=['GEO:N', 'Ratio:Q'],
+        color = alt.value("#228B22")
     ).properties(
-        title='Number of Men and Women in Executive Positions in {} Over the Years'.format(prov),
+        title='Ratio of Women v/s Men in Executive Positions in {} Over the Years'.format(prov),
         width=1200,
         height=200
     )
 
     if selected_year is not None:
-        rule = alt.Chart(pd.DataFrame({'selected_year': [selected_year]})).mark_rule(color='red').encode(
+        rule = alt.Chart(pd.DataFrame({'selected_year': [selected_year]})).mark_rule(color='gray').encode(
             x='selected_year:O'
         ).transform_filter(
             alt.FieldEqualPredicate(field='selected_year', equal=selected_year)
@@ -215,6 +229,14 @@ def create_chart(prov, selected_year):
         chart_with_marker = chart + rule
     else:
         chart_with_marker = chart
+    
+    canada_tot_ratio = alt.Chart(canada_total_ratio).mark_line(strokeDash=[1,1]).encode(
+        x='Year:O',
+        y='Ratio:Q',
+        color = alt.value("#800080")
+    )
+
+    chart_with_marker = chart_with_marker + canada_tot_ratio
 
     return chart_with_marker.configure_axis(
         labelAngle=0
--- src/line_chart.py
+++ src/line_chart.py
@@ -4,7 +4,10 @@ import dash_vega_components as dvc
 import altair as alt
 import pandas as pd
 
-df = pd.read_csv('data/filtered/pie_chart_data.csv')
+df = pd.read_csv('data/filtered/province_data.csv')
+card_data = pd.read_csv('data/filtered/cards_data.csv')
+time_columns = card_data['REF_DATE'].unique()
+province_columns = card_data['GEO'].unique()
 
 # Initiatlize the app
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -13,32 +16,67 @@ app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.layout = dbc.Container([
     dvc.Vega(id='line-chart', spec={}),
     html.P(html.Br()),
-    html.Label('Select an Industry'),
-    dcc.Dropdown(id='industry', options = df["Industry"].unique().tolist(), value ='Finance')
+    dbc.Label('Filter on Province'),
+    dcc.Dropdown(id='province-filter', options=province_columns, value='Canada, total'),
+    html.Br(),
+    dbc.Label('Filter on Year'),
+    dcc.Dropdown(id='year-filter', options=time_columns, value= 2016)
 ])
 
 # Server side callbacks/reactivity
 @callback(
     Output('line-chart', 'spec'),
-    Input('industry', 'value'),
+    [Input('province-filter', 'value'),
+     Input('year-filter', 'value')]
 )
+def create_chart(prov, selected_year):
+    filtered_df = df[(df["GEO"] == prov)]
 
-def create_chart(industry):
+    canada_total = {
+    'Year': [2016, 2017, 2018, 2019, 2020],
+    'Ratio': [0.422879, 0.424134, 0.432834, 0.433269, 0.457492]
+    }
+    canada_total_ratio = pd.DataFrame(canada_total)
 
-    filtered_df = df[(df["Industry"] == industry)]
+    prov_ratio_values = filtered_df.apply(lambda row: row['VALUE'] / filtered_df.loc[(filtered_df['REF_DATE'] == row['REF_DATE']) & (filtered_df['Gender'] == 'Men'), 'VALUE'].iloc[0], axis=1)
+    prov_ratio = prov_ratio_values[prov_ratio_values != 1]
 
-    chart = alt.Chart(filtered_df).mark_line().encode(
-        x = alt.X('REF_DATE:O', axis=alt.Axis(title='Year')),
-        y = alt.Y('VALUE:Q', axis=alt.Axis(title='Number of People')),
-        color = 'Gender:N',
-        tooltip = ['Gender:N', 'VALUE:Q']
+    province_gender_ratio = pd.DataFrame({
+    'Year': range(2016, 2021),
+    'Ratio': prov_ratio
+    })
+
+    chart = alt.Chart(province_gender_ratio).mark_line().encode(
+        x=alt.X('Year:O', axis=alt.Axis(title='Year')),
+        y=alt.Y('Ratio:Q', axis=alt.Axis(title='Ratio (Women/Men)')),
+        tooltip=['GEO:N', 'Ratio:Q'],
     ).properties(
-        title='Number of Men and Women in {} Over the Years'.format(industry),
-        width=600,
-        height=400
+        title='Ratio of Women v/s Men in Executive Positions in {} Over the Years'.format(prov),
+        width=1200,
+        height=200
     )
 
-    return chart.to_dict()
+    if selected_year is not None:
+        rule = alt.Chart(pd.DataFrame({'selected_year': [selected_year]})).mark_rule(color='red').encode(
+            x='selected_year:O'
+        ).transform_filter(
+            alt.FieldEqualPredicate(field='selected_year', equal=selected_year)
+        )
+        chart_with_marker = chart + rule
+    else:
+        chart_with_marker = chart
+    
+    canada_tot_ratio = alt.Chart(canada_total_ratio).mark_line(strokeDash=[1,1]).encode(
+        x='Year:O',
+        y='Ratio:Q'
+    )
+
+    chart_with_marker = chart_with_marker + canada_tot_ratio
+
+    return chart_with_marker.configure_axis(
+        labelAngle=0
+    ).to_dict()
+
 
 # Run the app/dashboard
 if __name__ == '__main__':
