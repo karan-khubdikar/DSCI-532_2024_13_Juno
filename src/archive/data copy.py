7239f44ed6d513dfd9cfc3127f9d802ba18a9d3d
scout-mckee <144981651+scout-mckee@users.noreply.github.com>
2024-04-16 16:11:47 -0700

    cleaned up file structure (#135)

---
+++ src/archive/data copy.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import geopandas as gpd
+
+
+df = pd.read_csv("../data/raw/filtered_canada.csv")
+
+new_df = df[(df["Size of enterprise"] == "Total all sizes") &
+            (df["Unit of measure"] == "Number") &
+            (df["Executive"] == "All officers\xa0") &
+            (df["GEO"] != "Unclassified province or territory")
+].copy()
+new_df = new_df.loc[:, ["REF_DATE", "Gender", "GEO", "VALUE", "Industry", "Type of corporation"]]
+
+new_df.to_csv('../data/filtered/filtered_data.csv', index=False)
+
+df = pd.read_csv('../data/filtered/filtered_data.csv')
+
+canadian_provinces = gpd.read_file('../data/filtered/map_chart_data.geojson')
